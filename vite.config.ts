2a2e93a1e2a1b9f0644e528dd34b8fbdf14c8603
hyc640110 <287127482+hyc640110@users.noreply.github.com>
2026-07-01 18:47:52 +0800

    Fix Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/00631L-Pro-Web-App/',
   plugins: [react()],
-  base: process.env.GITHUB_PAGES === 'true' ? '/Gpt-00631L/' : '/',
-  build: { outDir: 'dist', sourcemap: false }
-});
+})
