3922c0ad6201da994f4079f81fdb475f7e473832
hyc640110 <hyc640110@gmail.com>
2026-07-04 10:53:19 +0800

    Load v7.2 sync health module

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './v71-stable';
+import './v72-sync-health';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
 if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('./sw.js').catch(() => undefined));
